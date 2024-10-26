a9f71bd57583ddbd0260ee28307d85c29f3c01d6
Mauricio <soldadorasta23@gmail.com>
2024-10-25 21:41:00 -0300

    feat: first test

--- src/test/java/com/catalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/catalog/repositories/ProductRepositoryTests.java
@@ -6,7 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -23,6 +27,23 @@ public class ProductRepositoryTests {
         Optional<Product> result = repository.findById(existingId);
         //testa se o objeto está presente
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void testSaveProduct(){
+        Product product = new Product();
+        product.setName("Test Product");
+        product.setDescription("Test Description");
+        product.setPrice(5000.00);
+        product.setImgUrl("localhost/image");
+        product.setDate(Instant.now());
+
+        //salvar o o produto
+        Product savedProduct = repository.save(product);
+
+        //Asserts são para certificar se tudo deu certo
+        assertThat(savedProduct.getId()).isNotNull();//verifica se é não nulo
+        assertThat(savedProduct.getName()).isEqualTo("Test Product");
     }
 
 
